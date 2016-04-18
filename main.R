379f943bf98fbd654f4c986abb1df2248e9c674a
RomeroBarata <romero.barata@gmail.com>
2016-04-18 09:51:07 -0300

    Included data and code to perform the experiments

---
+++ main.R
@@ -0,0 +1,54 @@
+# Constants
+TRAIN_SEED <- 15476
+
+# Read data into the workspace
+concrete <- read.csv(file.path("data", "Concrete_Data.csv"), header = TRUE)
+names(concrete)[ncol(concrete)] <- "out"
+
+# Setup the caret control objects
+library(caret)
+fit_control <- trainControl(method = "repeatedcv", 
+                            number = 10, repeats = 5)
+
+# Enable for parallel computation
+# doMC::registerDoMC(cores = 3)
+
+# Train the glm model
+set.seed(TRAIN_SEED)
+glm_model <- train(out ~ ., data = concrete, 
+                   method = "glm", 
+                   trControl = fit_control, 
+                   preProc = c("center", "scale"))
+
+# Train the SVM model
+set.seed(TRAIN_SEED)
+svm_model <- train(out ~ ., data = concrete, 
+                   method = "svmRadial", 
+                   trControl = fit_control, 
+                   tuneLength = 3, 
+                   preProc = c("center", "scale"))
+
+# Define HyFIS parameters
+hyfis_params <- expand.grid(num.labels = 7, max.iter = 10)
+# Train the HyFIS model
+# No need to center and scale the features, since the HyFIS model code does it
+# automatically.
+set.seed(TRAIN_SEED)
+hyfis_model <- train(out ~ ., data = concrete, 
+                     method = "HYFIS", 
+                     trControl = fit_control, 
+                     tuneGrid = hyfis_params)
+
+# Resampled performance
+resamps <- resamples(list(GLM = glm_model, 
+                          SVM = svm_model, 
+                          HyFIS = hyfis_model))
+diffValues <- diff(resamps)
+
+# Print and plot performances
+summary(resamps)
+summary(diffValues)
+trellis.par.set(caretTheme())
+dotplot(resamps, metric = "RMSE")
+dotplot(resamps, metric = "Rsquared")
+dotplot(diffValues)
